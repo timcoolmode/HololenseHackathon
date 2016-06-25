40fad039a1559b3604400ab460da3cd8e46f45d2
bdemsky <bdemsky@uci.edu>
2016-06-25 16:57:07 -0700

    Towards minimap

---
+++ HololenseHackathonLimitedMobility/Assets/Scripts/MinimapCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapCamera : MonoBehaviour
+{
+
+    public GameObject target;
+
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+    }
+
+}
