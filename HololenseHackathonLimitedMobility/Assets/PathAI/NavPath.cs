e8618e57e6e9130562860a0ab6f5d0b900c5f734
Evan Bovie <evan@bovie.me>
2016-06-25 09:42:39 -0700

    structure of navpath

--- HololenseHackathonLimitedMobility/Assets/PathAI/NavPath.cs
+++ HololenseHackathonLimitedMobility/Assets/PathAI/NavPath.cs
@@ -1,6 +1,17 @@
-﻿using UnityEngine;using System.Collections;public class NavPath: MonoBehaviour {    public Transform target;    public GameObject trailObj;    NavMeshAgent agent;    bool hasPath = false;    NavMeshPath path = new NavMeshPath();
+﻿using UnityEngine;using System.Collections;public class NavPath: MonoBehaviour {    public Transform target;    public GameObject trailObj;    public float speed;    NavMeshAgent agent;    NavMeshPath path;    bool hasPath = false;
+    int currPoint = 0;
 
     // Use this for initialization
-    void Start () {        agent = GetComponent<NavMeshAgent>();                agent.CalculatePath(target.position, path);        if (path.status == NavMeshPathStatus.PathComplete) {            Instantiate(trailObj, transform.position, Quaternion.identity);            foreach (Vector3 corner in path.corners) {                Debug.Log(corner);            }        }    }		// Update is called once per frame	void Update () {        if(hasPath) {
-
-        }    }}
+    void Start () {        agent = GetComponent<NavMeshAgent>();        path = new NavMeshPath();        agent.CalculatePath(target.position, path);        if (path.status == NavMeshPathStatus.PathComplete) {            trailObj = (GameObject)Instantiate(trailObj, transform.position, Quaternion.identity);            hasPath = true;        }    }		// Update is called once per frame	void Update () {        if (hasPath) {
+            float step = speed * Time.deltaTime;
+            trailObj.transform.position = Vector3.MoveTowards(trailObj.transform.position, path.corners[currPoint], step);
+            if (trailObj.transform.position == path.corners[currPoint]) {
+                WaypointReached();
+            }
+        }    }    void WaypointReached() {
+        if (currPoint < (path.corners.Length - 1)) {
+            currPoint++;
+        } else {
+            // end reached
+        }
+    }}
