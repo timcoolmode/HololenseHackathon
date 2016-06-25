dd7680436313e2d4cf81c9ed8652bef0429a7973
Evan Bovie <evan@bovie.me>
2016-06-25 15:13:30 -0700

    Waypoints

---
+++ HololenseHackathonLimitedMobility/Assets/PathAI/ExplosionControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionControl : MonoBehaviour {
+    private ParticleSystem ps;
+
+    // Use this for initialization
+    void Start() {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (ps && !ps.IsAlive()) Destroy(gameObject);
+    }
+}
--- HololenseHackathonLimitedMobility/Assets/PathAI/NavPath.cs
+++ HololenseHackathonLimitedMobility/Assets/PathAI/NavPath.cs
@@ -1,17 +1,71 @@
-﻿using UnityEngine;using System.Collections;public class NavPath: MonoBehaviour {    public Transform target;    public GameObject trailObj;    public float speed;    NavMeshAgent agent;    NavMeshPath path;    bool hasPath = false;
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class NavPath: MonoBehaviour {
+    public Transform target;
+    public GameObject trailObj;
+    public GameObject waypointObj;
+    public GameObject debugObj;
+    public float speed;
+
+    NavMeshAgent agent;
+    NavMeshPath path;
+    GameObject[] waypoints;
+
+    bool hasPath = false;
     int currPoint = 0;
 
     // Use this for initialization
-    void Start () {        agent = GetComponent<NavMeshAgent>();        path = new NavMeshPath();        agent.CalculatePath(target.position, path);        if (path.status == NavMeshPathStatus.PathComplete) {            trailObj = (GameObject)Instantiate(trailObj, transform.position, Quaternion.identity);            hasPath = true;        }    }		// Update is called once per frame	void Update () {        if (hasPath) {
+    void Start () {
+        //agent = GetComponent<NavMeshAgent>();
+        //path = new NavMeshPath();
+        //agent.CalculatePath(target.position, path);
+        FindWaypoints();
+        InitializeAnimation();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (hasPath) {
             float step = speed * Time.deltaTime;
-            trailObj.transform.position = Vector3.MoveTowards(trailObj.transform.position, path.corners[currPoint], step);
-            if (trailObj.transform.position == path.corners[currPoint]) {
+            trailObj.transform.position = Vector3.MoveTowards(trailObj.transform.position, waypoints[currPoint].transform.position, step);
+            if (trailObj.transform.position == waypoints[currPoint].transform.position) {
                 WaypointReached();
             }
-        }    }    void WaypointReached() {
-        if (currPoint < (path.corners.Length - 1)) {
-            currPoint++;
-        } else {
-            // end reached
         }
-    }}
+    }
+
+    void WaypointReached() {
+        Instantiate(waypointObj, trailObj.transform.position, Quaternion.identity);
+        if (currPoint < (waypoints.Length - 1)) {
+            currPoint++;
+        }
+        else {
+            // end reached
+            hasPath = false;
+            RestartAnimation();
+        }
+    }
+
+    void InitializeAnimation() {
+        //if (path.status == NavMeshPathStatus.PathComplete) {
+            trailObj = (GameObject)Instantiate(trailObj, transform.position, Quaternion.identity);
+            hasPath = true;
+
+            foreach (GameObject waypoint in waypoints) {
+                Instantiate(debugObj, waypoint.transform.position, Quaternion.identity);
+            }
+        //}
+    }
+
+    void RestartAnimation() {
+        hasPath = true;
+        currPoint = 0;
+        InitializeAnimation();
+    }
+
+    void FindWaypoints() {
+        waypoints = GameObject.FindGameObjectsWithTag("Waypoint").OrderBy(go => go.name).ToArray();
+    }
+}
