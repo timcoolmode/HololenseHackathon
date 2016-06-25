a8b27b8be5197bf06ab067b350e3b7ab2a050fe8
Evan Bovie <evan@bovie.me>
2016-06-25 03:16:52 -0700

    add test character for AI and AI feedback work

--- HololenseHackathonLimitedMobility/Assets/PathAI/NavPath.cs
+++ HololenseHackathonLimitedMobility/Assets/PathAI/NavPath.cs
@@ -1,1 +1,6 @@
-﻿using UnityEngine;using System.Collections;public class NavPath: MonoBehaviour {    public Transform target;    NavMeshAgent agent;    // Use this for initialization    void Start () {        agent = GetComponent<NavMeshAgent>();        NavMeshPath path = new NavMeshPath();        agent.CalculatePath(target.position, path);        if (path.status == NavMeshPathStatus.PathComplete) {            foreach (Vector3 corner in path.corners) {                Debug.Log(corner);            }        }    }		// Update is called once per frame	void Update () {            }}
+﻿using UnityEngine;using System.Collections;public class NavPath: MonoBehaviour {    public Transform target;    public GameObject trailObj;    NavMeshAgent agent;    bool hasPath = false;    NavMeshPath path = new NavMeshPath();
+
+    // Use this for initialization
+    void Start () {        agent = GetComponent<NavMeshAgent>();                agent.CalculatePath(target.position, path);        if (path.status == NavMeshPathStatus.PathComplete) {            Instantiate(trailObj, transform.position, Quaternion.identity);            foreach (Vector3 corner in path.corners) {                Debug.Log(corner);            }        }    }		// Update is called once per frame	void Update () {        if(hasPath) {
+
+        }    }}
