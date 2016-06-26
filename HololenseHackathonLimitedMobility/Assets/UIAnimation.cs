fcc69e0689e7fe5bbc7dcd9d1e7ace8d7f0923d5
Tim@timcoolmode.com <tim@underminerstudios.com>
2016-06-26 09:09:51 -0700

    update to the UI

--- HololenseHackathonLimitedMobility/Assets/UIAnimation.cs
+++ HololenseHackathonLimitedMobility/Assets/UIAnimation.cs
@@ -2,14 +2,31 @@
 using System.Collections;
 
 public class UIAnimation : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
+    
+    
+    
+    // Use this for initialization
+    void Start () {
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        var Minimap = GameObject.Find("/MiniMap");
+        var Alert = GameObject.Find("/Alert");
+        
+
+        Alert.SetActive(true);
+
+        
+
+        if (Time.time == 15)
+        {
+
+        }
+        if (Time.time == 20)
+        {
+
+        }
 	}
 }
