cb2e56fbfe0455ac43ed3e0f2acb66f7c8d1e21d
Jack Baker <jack12354@gmail.com>
2016-06-25 20:49:12 -0700

    Add minimap animator scripts

---
+++ HololenseHackathonLimitedMobility/Assets/Scripts/AnimateMiniMap.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateMiniMap : MonoBehaviour
+{
+    public Animation anim;
+    int animId = 0;
+    public int delay = 0;
+    public string nameAnim;
+    bool isPlaying = false;
+
+    void Start()
+    {
+        if (this.anim == null)
+        {
+            anim = GetComponent<Animation>();
+        }
+    }
+
+    IEnumerator LetsWait(float secsToWait)
+    {
+        yield return new WaitForSeconds(secsToWait);  // we wait here (not exactly actually)...
+
+        // done waiting... now update the playing flag to go for the next animation.
+        isPlaying = false;
+    }
+
+    void Update()
+    {
+        if (!isPlaying)  // nothing playing? let's play something...
+        {
+            if (anim.isPlaying)
+            {
+                return;
+            }
+
+            switch (animId)
+            {
+                case 0:
+                    nameAnim = null;
+                    delay = 5;
+                    break;
+                case 1:
+                    nameAnim = "Minimap";
+                    delay = 2;
+                    break;
+                case 2:
+                    nameAnim = "MiniMapStay";
+                    delay = 2;
+                    break;
+                case 3:
+                    nameAnim = "MinimapGo";
+                    delay = 2;
+                    break;
+                default:
+                    isPlaying = true;
+                    return;
+            }
+
+            animId++;
+            isPlaying = true;
+
+            if (nameAnim != null)
+            {
+                anim.Play(nameAnim);
+            }
+            // now wait for the anim to finish plus 2 seconds more.
+            StartCoroutine(LetsWait(delay));
+        }
+    }
+}
